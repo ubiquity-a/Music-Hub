b4408b16fbb13d2b5b0b6c25efc6ab5e24f7becc
Akanksha <a7387917@gmail.com>
2023-02-13 16:43:22 +0530

    enable next

--- music.js
+++ music.js
@@ -2,31 +2,31 @@
 //initialize the variable
 let songIndex = 0;
 //let i;
-let audioElement = new Audio('m.mp3');
+let audioElement = new Audio('songs/1.mp3');
 let masterPlay = document.getElementById("masterPlay");
 let myProgressBar = document.getElementById('myProgressBar');
 let gif = document.getElementById('gif');
-//let songItems = Array.from(document.getElementsByClassName('songItem'));
+songItems = Array.from(document.getElementsByClassName('songItem'));
 let songs = [
-    {songName: "I don't Wanna talk", filePath: "spotify\m.mp3", coverPath: "back pic.jpg"},
-    {songName: " Scars To Your beautiful", filePath: "spotify\1.mp3", coverPath: "back pic.jpg"},
-    {songName: "Wasting My Emotion", filePath: "spotify\2.mp3", coverPath: "back pic.jpg"},
-    {songName: "Ankhein Teri Kitni Hasi ", filePath: "spotify\3.mp3", coverPath: "back pic.jpg"},
-    {songName: "Kash Aisa Hota ", filePath: "spotify\4.mp3", coverPath: "back pic.jpg"},
-    {songName: " Do You Know", filePath: "spotify\5.mp3", coverPath: "back pic.jpg"},
-    {songName: "Pretty Girl ", filePath: "spotify\6.mp3", coverPath: "back pic.jpg"},
-    {songName: "OK Not To Be OK", filePath: "spotify\7.mp3", coverPath: "back pic.jpg"},
-    {songName: "Braat", filePath: "spotify\8.mp3", coverPath: "back pic.jpg"}
+    {songName: "I don't Wanna talk", filePath: "songs/1.mp3", coverPath: "back pic.jpg"},
+    {songName: " Scars To Your beautiful", filePath: "songs/2.mp3", coverPath: "back pic.jpg"},
+    {songName: "Wasting My Emotion", filePath: "songs/3.mp3", coverPath: "back pic.jpg"},
+    {songName: "Ankhein Teri Kitni Hasi ", filePath: "songs/4.mp3", coverPath: "back pic.jpg"},
+    {songName: "Kash Aisa Hota ", filePath: "songs/5.mp3", coverPath: "back pic.jpg"},
+    {songName: " Do You Know", filePath: "songs/6.mp3", coverPath: "back pic.jpg"},
+    {songName: "Pretty Girl ", filePath: "songs/7.mp3", coverPath: "back pic.jpg"},
+    {songName: "OK Not To Be OK", filePath: "songs/8.mp3", coverPath: "back pic.jpg"},
+    {songName: "Braat", filePath: "songs/9.mp3", coverPath: "back pic.jpg"}
 
 ]
 
-//songItems.forEach((element, i)=> {
-  //  console.log(element, i);
-    //element.getElementsByTagName('img')[0].src = songs[i].coverPath;
-    //element.getElementsByClassName('songName')[0].innertext = songs[i].songName;
-//});
+    songItems.forEach((element, i)=> {
+    console.log(element, i);
+    element.getElementsByTagName('img')[0].src = songs[i].coverPath;
+    element.getElementsByClassName('songName')[0].innertext = songs[i].songName;
+   })
 
-//handle play/pause click
+  //handle play/pause click
 masterPlay.addEventListener('click', ()=>{
     if(audioElement.paused || audioElement.currentTime<=0){
         audioElement.play();
@@ -46,7 +46,7 @@ masterPlay.addEventListener('click', ()=>{
 
   audioElement.addEventListener('timeupdate', ()=>{
 
-  //update time on playing music
+  //update time on playing songs
   progress = parseInt((audioElement.currentTime/audioElement.duration)* 100);
   console.log(progress);
   
@@ -68,13 +68,47 @@ masterPlay.addEventListener('click', ()=>{
    Array.from(document.getElementsByClassName('songItemPlay')).forEach((element)=>{
         element.addEventListener('click', (e)=>{
             console.log(e.target);
+            index = parseInt(e.target.id);
             e.target.classlist.remove('fa-play-circle');
             e.target.classlist.add('fa-pause-circle');
-            audioElement.src = m.mp3;
+            audioElement.src = 'songs/${index+1}.mp3';
             audioElement.play();
             audioElement.currentTime = 0;
+            masterPlay.classList.remove('fa-play-circle');
+            masterPlay.classList.add('fa-pause-circle');
         })
 
    })
+
+   document.getElementById('next').addEventListener("click", ()=>{
+    if(songIndex>9){
+      songIndex = 0;
+
+    }
+    else{
+      songIndex+=1;
+    }
+    audioElement.src = 'songs/${index+1}.mp3';
+            audioElement.play();
+            audioElement.currentTime = 0;
+            masterPlay.classList.remove('fa-play-circle');
+            masterPlay.classList.add('fa-pause-circle');
+
+   })
+   document.getElementById('previous').addEventListener("click", ()=>{
+    if(songIndex<=0){
+      songIndex = 0;
+
+    }
+    else{
+      songIndex-=1;
+    }
+    audioElement.src = 'songs/${index+1}.mp3';
+            audioElement.play();
+            audioElement.currentTime = 0;
+            masterPlay.classList.remove('fa-play-circle');
+            masterPlay.classList.add('fa-pause-circle');
+
+  })
 
 
