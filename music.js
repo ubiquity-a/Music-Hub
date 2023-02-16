6b669df32c7776c2967af21da716ce85787bd546
Akanksha <a7387917@gmail.com>
2023-02-16 12:44:03 +0530

    list

--- music.js
+++ music.js
@@ -2,7 +2,7 @@
 //initialize the variable
 let songIndex = 0;
 //let i;
-let audioElement = new Audio('songs/1.mp3');
+let audioElement = new Audio('1.mp3');
 let masterPlay = document.getElementById("masterPlay");
 let myProgressBar = document.getElementById('myProgressBar');
 let gif = document.getElementById('gif');
